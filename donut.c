d0230bfc0af773f3d5785eda51e5604ce74cb464
Stridsvagn69420 <Stridsvagn69420@users.noreply.github.com>
2022-03-19 00:01:05 +0100

    Return 0 at SIGINT

--- donut.c
+++ donut.c
@@ -1,20 +1,27 @@
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
+#include <signal.h>
 
 int k;
-
 double sin(),cos();
 
+int run = 1;
+void  INThandler(int sig)
+{
+    run = 0;
+}
+
 int main() {
+	signal(SIGINT, INThandler);
 	float A = 0, B = 0, i, j , z[1760];
 	char b[1760];
 	printf("\n1b[2J");
-	for (;;) {
+	while(run) {
 		memset(b, 32, 1760);
 		memset(z, 0, 7040);
-		for (j = 0; 6.28 > j; j += 0.01) {
-			for(i = 0; 6.28 > i; i += 0.08) {
+		for (j = 0; 6.28 > j; j += 0.005) {
+			for(i = 0; 6.28 > i; i += 0.048) {
 				float c = sin(i), d = cos(j),
 					e = sin(A),
 					f = sin(j),
@@ -42,4 +49,5 @@ int main() {
 			A += 0.04;
 			B += 0.02;
 	}
+	return 0;
 }
