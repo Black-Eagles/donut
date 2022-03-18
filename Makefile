105c04dd96568d44eed97b939389bc4c1d908ac7
Stridsvagn69420 <Stridsvagn69420@users.noreply.github.com>
2022-03-18 23:33:35 +0100

    Makefiles

--- Makefile
+++ Makefile
@@ -1,2 +1,8 @@
 compile:
-	gcc -Wall -lm -g -o donut donut.c
+	@gcc -Wall -lm -g -o donut donut.c
+
+mingw64:
+	@x86_64-w64-mingw32-gcc -Wall -lm -g -o donut.exe donut.c
+
+mingw32:
+	@i686-w64-mingw32-gcc -Wall -lm -g -o donut-win32.exe donut.c
--- donut.c
+++ donut.c
@@ -15,9 +15,20 @@ int main() {
 		memset(z, 0, 7040);
 		for (j = 0; 6.28 > j; j += 0.01) {
 			for(i = 0; 6.28 > i; i += 0.08) {
-				float c = sin(i), d = cos(j), e = sin(A), f = sin(j), g = cos(A), h = d + 2, D = 1 / (c * h * e + f * g + 5), l = cos(i), m = cos(B), n = sin(B), t = c * h * g - f * e;
+				float c = sin(i), d = cos(j),
+					e = sin(A),
+					f = sin(j),
+					g = cos(A),
+					h = d + 2,
+					D = 1 / (c * h * e + f * g + 5),
+					l = cos(i),
+					m = cos(B),
+					n = sin(B),
+					t = c * h * g - f * e;
 				int x = 40 + 30 * D * (l * h * m - t * n);
-				int y = 12 + 15 * D * (l * h * n + t * m), o =x + 80 * y, N = 8 * ((f * e - c * d * g) * m - c * d * e - f * g - l * d * n);
+				int y = 12 + 15 * D * (l * h * n + t * m),
+					o =x + 80 * y,
+					N = 8 * ((f * e - c * d * g) * m - c * d * e - f * g - l * d * n);
 				if (22 > y && y > 0 && 80 > x && x > 0 && D > z[o]) {
 					z[o] = D;
 					b[o] = ".,-~:;=!*#$@:"[N > 0 ? N : 0];
